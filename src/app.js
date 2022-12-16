91504dccfca3c471104c82e77fd56e9847728a16
Raheem Idowu <idowu@princeton.edu>
2022-12-15 22:54:13 -0500

    final???

--- src/app.js
+++ src/app.js
@@ -1,73 +1,448 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
+import socketClient from 'socket.io-client';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
+import SKYBOXLOCATIONpx from '../skyboxes/Clear/vz_clear_right.png';
+import SKYBOXLOCATIONnx from '../skyboxes/Clear/vz_clear_left.png';
+import SKYBOXLOCATIONpy from '../skyboxes/Clear/vz_clear_up.png';
+import SKYBOXLOCATIONny from '../skyboxes/Clear/vz_clear_down.png';
+import SKYBOXLOCATIONpz from '../skyboxes/Clear/vz_clear_front.png';
+import SKYBOXLOCATIONnz from '../skyboxes/Clear/vz_clear_back.png';
+import TREEMODEL from '../Tree.glb';
+import TANKMODEL from '../tank.glb';
+import GRASSTEXTURE from '../grasstexture.png';
 
-const randomNameSelection = [
-  "Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett",
-  "Kilo", "Lima", "Mike", "November", 
-  "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey",
-  "X-ray", "Yankee", "Zulu"
-]
+//connect!
+const ENDPOINT = 'http://127.0.0.1:8081/';
+const SOCKET = socketClient(ENDPOINT);
 
-const connections = {};
-const activePlayers = [];
-const nameIndex = {};
-const score = {};
-let treePosition = [
-  [0,0], [10, 10], [32, 5], [16,3], [-10, -4], [14, -16],
-  [12, 24], [-17, 20], [-10, -4], [13, -9], [10, 13],
-  [1,4], [40, 9], [16, 29], [-23, 10], [-5, 2], [10, -11]];
+function sendUpdate(myself) {
+  const data = { position: myself.position, direction: myself.direction };
+  SOCKET.emit('clientUpdate', data);
+}
 
-server.listen(8081, () => {
-  console.log('listening on: *:8081');
-});
+let _doneLoading = false;
+let score = {};
+score["Local_Player"] = 0;
 
-io.on('connection', (socket) => {
-  let newPlayerName = randomNameSelection[Math.floor(Math.random() * randomNameSelection.length)];
-  while (nameIndex[newPlayerName]) {
-    newPlayerName = randomNameSelection[Math.floor(Math.random() * randomNameSelection.length)];
+// add a new player to the scene
+function addPlayer(p, n, s) {
+  let playerColor = p[n].color;
+  if (playerColor === undefined) playerColor = 0x00ff00;
+  const mesh = new THREE.Object3D().copy(_tankModel);
+  mesh.traverse((object) => {
+    object.name = n;
+  });
+  mesh.position.copy(p[n].position);
+  mesh.name = n;
+  mesh.rotation.copy(new THREE.Euler(0, p[n].direction, 0));
+  s.add(mesh);
+  p[n].object = mesh;
+}
+
+let trees = [];
+let _treeModel;
+
+// add tree
+function addTree(x, z, s) {
+  const newTree = new THREE.Object3D().copy(_treeModel);
+  newTree.traverse((object) => {
+    object.name = "Tree";
+  });
+  newTree.scale.multiplyScalar(0.4);
+  newTree.position.set(x, 0, z);
+  // trees can have a random orientation
+  trees.push([x, z]);
+  newTree.rotateY(Math.random() * 2 * Math.PI);
+  s.add(newTree);
+}
+
+// update a player who has already been added
+function updatePlayer(p, n) {
+  p[n].object.position.copy(p[n].position);
+  p[n].object.rotation.copy(new THREE.Euler(0, p[n].direction, 0));
+}
+
+// set up a renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+document.body.style.margin = 0;
+document.body.style.overflow = 'hidden';
+
+// add static image (crosshair)
+/*
+const crossHair = new Image();
+crossHair.src = CROSSHAIRIMAGE;
+document.body.appendChild(crossHair);
+crossHair.style.position = 'fixed';
+crossHair.style.left = '50%';
+crossHair.style.top = '50%';
+crossHair.style.marginLeft = '-32px';
+crossHair.style.marginTop = '-32px';
+*/
+
+// create the hud
+const hud = document.createElement("div");
+hud.style.position = "fixed";
+hud.style.right = "2%";
+hud.style.top = "3%"
+hud.style.width = "15%";
+hud.style.backgroundColor = "gray";
+hud.style.border = "1px solid black";
+const whoami = document.createElement("p");
+whoami.innerText = "not yet connected";
+whoami.style.fontSize = "32px";
+whoami.style.textAlign = "center";
+hud.appendChild(whoami);
+const enemies = document.createElement("p");
+enemies.innerHTML = "Scoreboard:"
+enemies.style.fontSize = "28px";
+enemies.style.textAlign = "center";
+hud.appendChild(enemies);
+document.body.appendChild(hud);
+
+// game notifications hud element
+const notif = document.createElement("p");
+notif.style.position = "fixed";
+notif.style.right = "20%";
+notif.style.top = "10%";
+notif.innerHTML = "";
+notif.style.fontSize = "40px";
+notif.style.fontWeight = "bold";
+notif.style.textAlign = "right";
+document.body.appendChild(notif);
+
+// reload hud element
+const reloadtext = document.createElement("p");
+reloadtext.style.position = "fixed";
+reloadtext.style.textAlign = "right";
+reloadtext.style.right = "20%";
+reloadtext.style.top = "0%";
+reloadtext.style.color = "green";
+reloadtext.style.fontSize = "50px";
+reloadtext.style.fontWeight = "bold";
+reloadtext.innerHTML = "Laser READY";
+reloadtext.style.backdropFilter = "blur(5px)";
+reloadtext.style.borderRadius = "20px";
+reloadtext.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+reloadtext.style.padding = "10px";
+document.body.appendChild(reloadtext);
+
+// set up a camera
+const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 100);
+camera.position.z = -10;
+camera.position.y = 10;
+camera.lookAt(new THREE.Vector3(0,0,0));
+let cameraYaw = 0; // amount we look up = rotate camera by perp axis, radians
+
+// set up orbit controls for debugging
+/*
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, 0);
+*/
+// add axes helpers
+// scene.add(new THREE.AxesHelper(5));
+
+// set up a light
+const light = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
+const light2 = new THREE.PointLight( 0xffffff, 1, 0 );
+light2.position.set(0, 100, 0);
+
+// set up a scene and give it a skybox
+const loader = new THREE.CubeTextureLoader();
+const textureCube = loader.load([
+  SKYBOXLOCATIONpx, SKYBOXLOCATIONnx,
+  SKYBOXLOCATIONpy, SKYBOXLOCATIONny,
+  SKYBOXLOCATIONpz, SKYBOXLOCATIONnz,
+]);
+const scene = new THREE.Scene();
+scene.background = textureCube;
+scene.add(light);
+scene.add(light2);
+
+
+
+// the plane of exsistence
+const textureLoader = new THREE.TextureLoader();
+const grassTexture = textureLoader.load(GRASSTEXTURE);
+grassTexture.wrapS = THREE.RepeatWrapping;
+grassTexture.wrapT = THREE.RepeatWrapping;
+grassTexture.repeat.set(10, 10);
+console.log(grassTexture);
+const planeGeometry = new THREE.PlaneGeometry(130,130);
+const planeMaterial = new THREE.MeshBasicMaterial({ map: grassTexture, side: THREE.DoubleSide});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+planeMesh.lookAt(0, 10, 0);
+scene.add(planeMesh);
+
+// add stats to the screen
+const stats = Stats();
+document.body.appendChild(stats.dom);
+
+// load my assets
+const gltfLoader = new GLTFLoader();
+let _tankModel;
+// load the characters
+gltfLoader.load(
+  TANKMODEL,
+  (tankGLTF) => {
+    _tankModel = tankGLTF.scene;
+    console.log(tankGLTF);
+    gltfLoader.load(
+      TREEMODEL, 
+      (treeGLFT => {
+        _treeModel = treeGLFT.scene;
+        console.log(treeGLFT);
+        loadingProgress++;
+        checkLoading();
+      })
+    )
   }
-  console.log('A player joined', socket.id, "and given name", newPlayerName);
-  activePlayers.push(newPlayerName);
-  nameIndex[socket.id] = newPlayerName;
-  socket.emit("yourName", newPlayerName);
-  socket.emit("treePositions", treePosition);
-  socket.emit("currentScore", score);
-  score[newPlayerName] = 0;
+);
 
-  socket.on('clientUpdate', (data) => {
-  // send this players update to all other players
-  // store out latest data on this guy
-    connections[socket.id] = data;
-    socket.broadcast.emit('PlayerUpdate', { ...data, name: nameIndex[socket.id]});
-  });
+// server will send update of player objects with name, posisition, and direction vector
+// we send our location and direction too, every tick
+// whenever we get one, we update that players position in the our world
+// when a new player joins we need to make that players object
 
-  socket.on('PlayerShooting', () => {
-    socket.broadcast.emit('PlayerShooting', nameIndex[socket.id]);
-    console.log(nameIndex[socket.id] + " is shooting!");
-  });
+function updateScoreboard() {
+  enemies.innerHTML = "Scoreboard:<br>";
+  for (let k in players) {
+    if (k === 'Local_Player') enemies.innerHTML += _myName + ": " + score[k] + "<br>";
+    else enemies.innerHTML += k + ": " + score[k] + "<br>";
+  }
+}
 
-  socket.on('disconnect', () => {
-    const playerName = nameIndex[socket.id];
-    delete score[playerName];
-    console.log("player", socket.id, "with name", playerName, "left");
-    connections[playerName] = null;
-    nameIndex[socket.id] = null;
-    activePlayers.splice(activePlayers.indexOf(playerName), 1);
-    console.log(activePlayers);
-    socket.broadcast.emit('PlayerDisconnect', playerName);
-  });
+let treePositions;
+const SPEED = 0.1;
+const keyMap = new Map();
+const players = {};
+let _myName;
+const notifqueue = [];
 
-  socket.on("PlayerShot", (name) => {
-    // broadcast and kill
-    const shooter = nameIndex[socket.id];
-    console.log(nameIndex[socket.id], "shot", name);
-    io.emit("PlayerShot", [shooter, name]); //to everyone, including the sender
-    score[shooter]++;
-    score[name] = 0;
-  });
+function clearNotif() {
+  notif.innerHTML = "";
+  runNotif();
+}
+
+function runNotif() {
+  // already a notification displaying, we will be called once it gets cleared
+  if (!notif.innerHTML) {
+    if (notifqueue.length === 0) return; // no notifications to display right now
+    notif.innerHTML = notifqueue.shift(); // remove the first element and get it
+    window.setTimeout(clearNotif, 1500);
+  }
+}
+
+// code for updating players
+SOCKET.on('PlayerUpdate', (msg) => {
+  if (!_doneLoading) return;
+  if (players[msg.name] === undefined) {
+    players[msg.name] = msg;
+    //make a new player with this
+    addPlayer(players, msg.name, scene);
+    if (score[msg.name] === undefined) score[msg.name] = 0;
+    //update the scoreboard
+    updateScoreboard();
+    //make a notification
+    notifqueue.push(msg.name + " joined!");
+    runNotif();
+  } else {
+    players[msg.name].direction = msg.direction;
+    players[msg.name].position = msg.position;
+  }
+  updatePlayer(players, msg.name);
 });
+
+SOCKET.on("yourName", (name) => {
+  _myName = name;
+  whoami.innerHTML = "You are: " + _myName;
+  updateScoreboard();
+  console.log(name);
+});
+
+SOCKET.on("PlayerDisconnect", (name) => {
+  scene.remove(players[name].object);
+  delete players[name];
+  delete score[name];
+  notifqueue.push(name + " left.");
+  runNotif();
+  updateScoreboard();
+});
+
+SOCKET.on("PlayerShooting", (name) => {
+  renderLaser(players, name);
+});
+
+SOCKET.on("PlayerShot", (names) => {
+  notifqueue.push(names[0] + " killed " + names[1]);
+  runNotif();
+  if (_myName === names[0]) score['Local_Player']++;
+  else score[names[0]]++;
+  // was it me who died
+  if (_myName == names[1]) {
+    spawnPlayer();
+    score['Local_Player'] = 0;
+  } else {
+    score[names[1]] = 0;
+  }
+  updateScoreboard();
+});
+
+SOCKET.on("currentScore", (serverScores) => {
+  score = {...score, ...serverScores};
+  loadingProgress++;
+  checkLoading();
+});
+
+SOCKET.on("treePositions", (treePos) => {
+  treePositions = treePos;
+  loadingProgress++;
+  checkLoading();
+});
+
+document.onkeydown = function (e) {
+  keyMap.set(e.key, true);
+};
+
+document.onkeyup = function (e) {
+  keyMap.set(e.key, false);
+};
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max-min)) + min;
+}
+
+function spawnPlayer() {
+  players['Local_Player'].position.set(getRandomInt(-30, 30), 0.31, getRandomInt(-30, 30));
+  players['Local_Player'].position.add(new THREE.Vector3(0.5, 0, 0.5));
+  console.log(players['Local_Player'].position);
+}
+
+let loadingProgress = 0;
+
+function checkLoading() {
+  console.log("checkloading", loadingProgress);
+  if (loadingProgress === 3) {
+    doneLoading();
+    console.log("done loading");
+  }
+}
+
+function doneLoading() {
+  _doneLoading = true;
+  // position is game world posiiton, and direction is angle of rotation with respect to z axis
+  players['Local_Player'] = { position: new THREE.Vector3(0, 100, 0), direction: 0, color: 0xff0000};
+  // hide you in the sky
+  addPlayer(players, 'Local_Player', scene);
+  for (const x of treePositions) addTree(x[0], x[1], scene);
+  renderer.setAnimationLoop(render);
+  //actually spawn you on the ground
+  spawnPlayer();
+  updateScoreboard();
+}
+
+let reloading = false;
+const LASER_RANGE = 15;
+
+const lasermaterial = new THREE.LineBasicMaterial( {color: 0xff0000, linewidth: 4} );
+function renderLaser(p, n) {
+  const tankPosition = new THREE.Vector3(p[n].position.x, p[n].position.y, p[n].position.z);
+  tankPosition.add(new THREE.Vector3(0,0.22,0));
+  const upVector = new THREE.Vector3(0,1,0);
+  const zVector = new THREE.Vector3(0,0,1);
+  const targetPosition = tankPosition.clone().add(zVector.applyAxisAngle(upVector, p[n].direction)
+    .multiplyScalar(LASER_RANGE));
+  const geometry = new THREE.BufferGeometry().setFromPoints([tankPosition, targetPosition]);
+  const line = new THREE.Line(geometry, lasermaterial);
+  scene.add(line);
+  window.setTimeout((toRemove) => {scene.remove(toRemove); }, 100, line);
+  return [tankPosition, targetPosition, line];
+}
+
+function shootLaser() {
+  SOCKET.emit("PlayerShooting");
+  const positions = renderLaser(players, 'Local_Player');
+  reloading = true;
+  reloadtext.innerHTML = "Laser DOWN";
+  reloadtext.style.color = "red";
+  window.setTimeout(()=>{reloading = false; reloadtext.innerHTML = "Laser READY";
+    reloadtext.style.color = "darkgreen"}, 2000);
+  // check whether you hit anyone haiyaaaa this is so much work
+  const raycaster = new THREE.Raycaster(positions[0], positions[1].sub(positions[0]).normalize(), 0, LASER_RANGE);
+  const sceneObjects = [...scene.children];
+  sceneObjects.splice(sceneObjects.indexOf(players['Local_Player'].object), 1);
+  sceneObjects.splice(sceneObjects.indexOf(positions[2]), 1);
+  const intersects = raycaster.intersectObjects(sceneObjects, true);
+  const alreadyKilled = {};
+  for (const intersection of intersects) {
+    let intersectName = intersection.object.name;
+    if (intersectName === "Tree") break; // trees are strong
+    if (players[intersectName] && !alreadyKilled[intersectName]) {
+      SOCKET.emit("PlayerShot", intersectName);
+      alreadyKilled[intersectName] = true;
+    }
+  }
+  
+}
+
+function render( time ) {
+  const me = players['Local_Player'];
+
+  // update player position by movement
+  const moveForward = me.object.getWorldDirection(new THREE.Vector3()).clone().normalize().multiplyScalar(SPEED);
+  const moveRight = moveForward.clone().cross(new THREE.Vector3(0, 1, 0)).normalize().multiplyScalar(SPEED);
+  if (keyMap.get('a')) me.position.sub(moveRight);
+  if (keyMap.get('d')) me.position.add(moveRight);
+  if (keyMap.get('w')) me.position.add(moveForward);
+  if (keyMap.get('s')) me.position.sub(moveForward);
+  if (keyMap.get('ArrowUp')) if (!reloading) shootLaser();
+  if (keyMap.get('ArrowRight')) players['Local_Player'].direction -= 0.03;
+  if (keyMap.get('ArrowLeft')) players['Local_Player'].direction += 0.03;
+  
+  //make sure my tank doesn't leave map (invisible walls)
+  if (me.position.x > 40) me.position.x = 40;
+  if (me.position.x < -40) me.position.x = -40;
+  if (me.position.z < -40) me.position.z = -40;
+  if (me.position.z > 40) me.position.z = 40;
+
+  // make sure my tank doesn't go into any trees
+  for (let i = 0; i<trees.length; i++) {
+    let tree = trees[i];
+    let treePosition = new THREE.Vector2(tree[0], tree[1]);
+    let tankPosition = new THREE.Vector2(me.position.x, me.position.z);
+    let deltaPosition = tankPosition.clone().sub(treePosition);
+    if (deltaPosition.length() < 1.1) {
+      // collision
+      tankPosition.copy(treePosition).add(deltaPosition.normalize().multiplyScalar(1.11));
+      me.position.x = tankPosition.x;
+      me.position.z = tankPosition.y;
+    }
+  }
+
+  /* mouse based controls
+  cameraYaw %= 2 * Math.PI;
+  cameraYaw -= (mouseYDelta / 500) * 2;
+  players['Local_Player'].direction -= (msouseXDelta / 4000) * 6.28;
+  players['Local_Player'].direction %= 2 * Math.PI;
+
+  if (cameraYaw > 45 * (Math.PI / 180)) cameraYaw = 45 * (Math.PI / 180);
+  if (cameraYaw < -45 * (Math.PI / 180)) cameraYaw = -45 * (Math.PI / 180);
+  mouseXDelta = 0;
+  mouseYDelta = 0;
+  */
+
+  updatePlayer(players, 'Local_Player');
+  sendUpdate(me);
+
+  camera.position.copy(me.position).add(moveForward);
+  camera.position.add(new THREE.Vector3(0,0.4,0));
+  camera.lookAt(camera.position.clone().add(moveForward));
+
+  //controls.update();
+  renderer.render(scene, camera);
+  stats.update();
+}
--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
-const { readFileSync } = require('fs');
-const https = require('https');
+const express = require('express');
+const http = require('http');
 const { Server } = require('socket.io');
+const { Points } = require('three');
 
-const server = https.createServer(
-  {
-    key: readFileSync("../key.pem"),
-    cert: readFileSync("../cert.pem"),
-  },
-);
-
-const io = new Server(server);
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
 
 const randomNameSelection = [
   "Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett",
@@ -18,14 +14,104 @@ const randomNameSelection = [
   "X-ray", "Yankee", "Zulu"
 ]
 
+const GAMESIZE = 40 //map is 40 in +x, -x, +y, -y
+let cellsize;
+
+// make all points positive and then shift by -40, -40 afterwards
+function insertPointIntoGrid(grid, point) {
+  let gridx = Math.floor(point[0] / cellsize);
+  let gridy = Math.floor(point[1] / cellsize);
+  grid[gridx][gridy] = point;
+}
+
+function distance(x0, y0, x1, y1){
+  return Math.sqrt((x1 - x0)*(x1-x0) + (y1 - y0)*(y1-y0));
+}
+
+function validPoint(grid, gsize, p, radius) {
+
+  if (p[0] < 0 || p[0] >= GAMESIZE*2 || p[1] < 0 || p[1] >= GAMESIZE*2) {
+    return false;
+  }
+
+  let xindex = Math.floor(p[0] / cellsize);
+  let yindex = Math.floor(p[1] / cellsize);
+  let i0 = Math.max(xindex - 1, 0);
+  let i1 = Math.min(xindex + 1, gsize - 1);
+  let j0 = Math.max(yindex - 1, 0);
+  let j1 = Math.min(yindex + 1, gsize - 1);
+
+  for (let i = i0; i<=i1; i++){
+    for (let j = j0; j<=j1; j++){
+      if (grid[i][j] !== undefined){
+        if (distance(grid[i][j][0], grid[i][j][1], p[0], p[1] < radius)) return false;
+      }
+    }
+  }
+
+  return true;
+
+}
+
+function poissonDiskSampling(radius, k) {
+  const final_points = [];
+  const active_points = [];
+  cellsize = Math.floor(radius / Math.sqrt(2));
+  const gridsize = Math.ceil((2 * GAMESIZE) / cellsize) + 1;
+
+  const grid = new Array(gridsize);
+  for (let i = 0; i < gridsize; i++) {
+    grid[i] = new Array(gridsize);
+  }
+
+  const p0 = [Math.random()*GAMESIZE*2,
+    Math.random()*GAMESIZE*2];
+  
+  insertPointIntoGrid(grid, p0);
+  final_points.push(p0);
+  active_points.push(p0);
+
+  while (active_points.length > 0) {
+    let random_index = Math.floor(active_points.length * Math.random());
+    let current_point = active_points[random_index];
+
+    let found = false;
+    for (let i = 0; i<k; i++) {
+
+      let newAngle = Math.floor(2 * Math.PI * Math.random());
+      let newRadius = Math.floor(radius * Math.random() + radius);
+      let newx = current_point[0] + newRadius * Math.cos(newAngle);
+      let newy = current_point[1] + newRadius * Math.sin(newAngle);
+      let newPoint = [newx, newy];
+
+      //check neighboring 8 cells
+      if (!validPoint(grid, gridsize, newPoint, radius)) continue;
+
+      final_points.push(newPoint);
+      insertPointIntoGrid(grid, newPoint);
+      active_points.push(newPoint);
+      found = true;
+      break;
+      
+    }
+    if (!found) active_points.splice(random_index, 1);
+  }
+
+  for (let i = 0; i<final_points.length; i++) {
+    final_points[i][0] -= GAMESIZE;
+    final_points[i][1] -= GAMESIZE;
+  }
+  
+  return final_points;
+
+}
+
+
 const connections = {};
 const activePlayers = [];
 const nameIndex = {};
 const score = {};
-let treePosition = [
-  [0,0], [10, 10], [32, 5], [16,3], [-10, -4], [14, -16],
-  [12, 24], [-17, 20], [-10, -4], [13, -9], [10, 13],
-  [1,4], [40, 9], [16, 29], [-23, 10], [-5, 2], [10, -11]];
+let treePosition = poissonDiskSampling(4,30);
 
 server.listen(8081, () => {
   console.log('listening on: *:8081');
